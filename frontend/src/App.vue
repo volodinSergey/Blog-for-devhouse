9a520d1638b96cc24928df0cae5df35f74bb591d
Volodin Sergey <volodin7ergey@icloud.com>
2022-12-29 22:41:07 +0300

    Init commit. Installed vue , prettien and eslint config

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    id="app"
+    class="red"
+  >
+    <h1>Hello from vue !!!!!!!</h1>
+
+    <input
+      type="text"
+      value="Heloo"
+      placeholder="Type anything here...."
+    />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
