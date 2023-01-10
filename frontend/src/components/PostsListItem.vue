39e78fb51ecefff936f9920e54fc085fb0c17814
Volodin Sergey <volodin7ergey@icloud.com>
2023-01-10 21:24:02 +0300

    Added basic add commemt ui

--- frontend/src/components/PostsListItem.vue
+++ frontend/src/components/PostsListItem.vue
@@ -48,19 +48,71 @@
       />
     </div>
 
-    <button
-      class="show-comments-button"
-      v-show="post.comments?.length"
-      @click="toggleCommentsShowing"
-    >
-      {{ toggleCommentsButtonText }}
-    </button>
+    <div class="post-item__comments-section">
+      <button
+        class="show-comments-button"
+        v-show="post.comments?.length"
+        @click="toggleCommentsShowing"
+      >
+        {{ toggleCommentsButtonText }}
+      </button>
 
-    <CommentsList
-      v-if="post.comments?.length"
-      :commentsShowed="commentsShowed"
-      :comments="post.comments"
-    />
+      <CommentsList
+        v-if="post.comments?.length"
+        :commentsShowed="commentsShowed"
+        :comments="post.comments"
+      />
+
+      <form class="add-comment-form">
+        <BaseTextField placeholder="Add your comment" />
+        <BaseButtton>
+          <svg
+            class="add-comment-icon"
+            width="64px"
+            height="64px"
+            viewBox="-2.4 -2.4 28.80 28.80"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#af7474"
+          >
+            <g
+              id="SVGRepo_bgCarrier"
+              stroke-width="0"
+            />
+
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke="#CCCCCC"
+              stroke-width="0.144"
+            />
+
+            <g id="SVGRepo_iconCarrier">
+              <g id="style=linear">
+                <g id="email">
+                  <path
+                    id="vector"
+                    d="M17 20.5H7C4 20.5 2 19 2 15.5V8.5C2 5 4 3.5 7 3.5H17C20 3.5 22 5 22 8.5V15.5C22 19 20 20.5 17 20.5Z"
+                    stroke="#242bff"
+                    stroke-width="1.5"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    id="vector_2"
+                    d="M18.7698 7.7688L13.2228 12.0551C12.5025 12.6116 11.4973 12.6116 10.777 12.0551L5.22998 7.7688"
+                    stroke="#242bff"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                  />
+                </g>
+              </g>
+            </g></svg
+        ></BaseButtton>
+      </form>
+    </div>
   </li>
 </template>
 
@@ -202,5 +254,23 @@ export default {
       background-color: #00000026;
     }
   }
+}
+
+.add-comment-form {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 10px;
+  border: 1px solid #23243e;
+  border-radius: 10px;
+  gap: 1rem;
+  margin-top: 1rem;
+}
+
+.add-comment-icon {
+  width: 40px;
+  height: 40px;
+  stroke: red;
+  cursor: pointer;
 }
 </style>
