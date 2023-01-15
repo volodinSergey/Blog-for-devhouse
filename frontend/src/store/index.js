a049962025e0d55ab458b07c07ba45649a2c9bf5
Volodin Sergey <volodin7ergey@icloud.com>
2023-01-15 18:58:15 +0300

    Render users list without current user

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from '@/store/modules/auth.module'
-import likes from '@/store/modules/likes/likes.module'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth,
-    likes
+    auth
   },
 })
