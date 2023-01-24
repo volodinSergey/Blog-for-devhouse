596e7bfe6074c2088ec04b55e11cbf22a34cf4e7
Volodin Sergey <volodin7ergey@icloud.com>
2023-01-24 15:56:40 +0300

    Edited strapi settings

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'someSecret'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT', 'someSalt'),
